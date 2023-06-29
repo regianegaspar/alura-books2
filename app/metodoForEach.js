2dedd17ebb44ca5def5aa7a5af0e89edb9ab13f8
Regiane Gaspar <123254220+reggaspar@users.noreply.github.com>
2023-06-29 08:41:20 -0300

    filtrando os livros por categoria

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivroNaTela(listaDeLivros){
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
